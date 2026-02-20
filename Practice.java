6e73831f5979f3530f3334922e6cdfbffcf387a0
RahulWissen <RahulPrasad.Singh@wissen.com>
2026-02-20 16:10:33 +0530

    Resolved 1

--- Practice.java
+++ Practice.java
@@ -1,7 +1,8 @@
 public class Practice {
     public static void main(String[] args) {
 
-        System.out.println("Hello from Master branch...");             System.out.println("Hello from test-branch...");
+        System.out.println("Hello from Master branch...");            
+        System.out.println("Hello from test-branch...");
 
     }
 }
